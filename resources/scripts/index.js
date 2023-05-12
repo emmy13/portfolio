e75f04a9c6beccf48bad55283a1f5e5d30db9309
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2023-05-12 15:48:38 +0100

    skill section

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -25,7 +25,7 @@ function animation() {
     let shuffledArr = gsap.utils.shuffle(spanArr);
 
     gsap.to(shuffledArr, { duration: 1, y: -currentHeight, stagger: 0.2, delay: 2, ease: Expo.easeOut, onComplete: animation })
-    if (currentHeight / textHeight == ((words.length/textPerLine) - 1)) currentHeight = 0;
+    if (currentHeight / textHeight == ((words.length / textPerLine) - 1)) currentHeight = 0;
     else currentHeight += textHeight;
 }
 
@@ -70,13 +70,91 @@ function adjustHeight() {
     parallaxText.style.height = `${textHeight}px`
 }
 
+//Skills
+const skillsBox = document.querySelector(".skills--box-skills");
+const skills = [
+    {
+        logo: 'html.png',
+        color: 'rgba(230, 81, 0)',
+        name: 'html 5',
+        text: 'standard markup language for web pages',
+        shadow: 'rgba(230, 81, 0, 0.15)'
+    },
+    {
+        logo: 'css.png',
+        color: 'rgb(2, 119, 189)',
+        name: 'css 3',
+        text: 'style sheet language for HTML documents',
+        shadow: 'rgba(2, 119, 189, 0.15)'
+    },
+    {
+        logo: 'js.png',
+        color: 'rgb(255, 214, 0)',
+        name: 'javascript',
+        text: 'web programming language',
+        shadow: 'rgba(255, 214, 0, 0.15)'
+    },
+    {
+        logo: 'node.png',
+        color: 'rgb(56, 142, 60)',
+        name: 'node js',
+        text: 'open source server environment',
+        shadow: 'rgba(56, 142, 60, 0.15)'
+    },
+    {
+        logo: 'express.png',
+        color: 'rgb(33, 163, 102)',
+        name: 'express js',
+        text: 'backend node js framework',
+        shadow: 'rgba(33, 163, 102, 0.15)'
+    },
+    {
+        logo: 'sql.png',
+        color: 'rgb(0, 121, 107)',
+        name: 'MySQL',
+        text: 'relational database management system',
+        shadow: 'rgba(0, 121, 107, 0.15)'
+    }
+]
+
+function attachSkills() {
+    for (let i = 0; i < skills.length; i++) {
+        let div = document.createElement("div");
+        let divContent = `<div class="skills-logo">
+                <img src="./resources/icons/${skills[i].logo}" alt="logo">
+            </div>
+            <h1 style="color: ${skills[i].color}">${skills[i].name}</h1>
+            <p>${skills[i].text}</p>`;
+        div.innerHTML = divContent;
+        div.classList.add("skills-skill");
+        div.style.border = `1px solid ${skills[i].color}`;
+        div.style.boxShadow = `${skills[i].shadow} 0px 4px 24px`;
+
+        skillsBox.append(div);
+    }
+}
+
+function adjustSkills() {
+    let gap = 30;
+    let spacing = 0;
+    const skillsSkill = document.querySelectorAll(".skills-skill");
+    const skillWidth = skillsSkill[0].clientWidth + gap;
+    for (let i = 0; i < skillsSkill.length; i++) {
+        skillsSkill[i].style.transform = `translateX(${spacing}px)`;
+        spacing+=skillWidth;
+    }
+}
+
+attachSkills();
+adjustSkills();
+
 let windowWidth = window.innerWidth;
 let resizeTimeout;
 
 window.onresize = () => {
     clearTimeout(resizeTimeout)
     resizeTimeout = setTimeout(() => {
-        if(window.innerWidth !== windowWidth) {
+        if (window.innerWidth !== windowWidth) {
             adjustHeight();
             windowWidth = window.innerWidth;
         }
